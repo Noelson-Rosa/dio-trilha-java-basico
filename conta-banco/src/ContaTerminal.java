bb8f60a8744cf9489512a64f1caa333471a454c9
Noelson Rosa <41403798+Noelson-Rosa@users.noreply.github.com>
2024-02-02 17:15:15 -0300

    finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         
+        Scanner leia = new Scanner(System.in);
+        
+        int numero;
+        String agencia;
+        String nome_cliente;
+        double saldo = 0;
+
+        System.out.println(" Informe o Nome do Cliente: ");
+        nome_cliente = leia.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        agencia=leia.nextLine();
+
+        System.out.println("Informe o numero da conta: ");
+        numero = leia.nextInt();
+        
+        
+
+        System.out.println("Informe o saldo: ");
+        saldo = leia.nextDouble();
+
+        System.out.println("Olá ".concat(nome_cliente) );
+        System.out.println(" obrigado por criar uma conta em nosso banco a sua agencia é  ".concat(agencia)+ " e o numero é " + numero + " o Saldo é " + saldo);
+
+        
+
+
+        leia.close();
     }
 }
